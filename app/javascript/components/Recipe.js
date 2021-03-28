9694fcc0805b4cebb3c5c3a79a277aae57eca8f0
Ramin Mammadzada <remki-design@hotmail.com>
2021-03-28 17:16:52 +0300

    Add delete method to Recipe component.

--- app/javascript/components/Recipe.js
+++ app/javascript/components/Recipe.js
@@ -5,8 +5,34 @@ class Recipe extends React.Component {
     constructor(props) {
         super(props);
         this.state = { recipe: { ingredients: "" } };
-
         this.addHtmlEntities = this.addHtmlEntities.bind(this);
+        this.deleteRecipe = this.deleteRecipe.bind(this);
+    }
+
+    deleteRecipe() {
+        const {
+            match: {
+                params: { id }
+            }
+        } = this.props;
+        const url = `/api/v1/destroy/${id}`;
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+
+        fetch(url, {
+            method: "DELETE",
+            headers: {
+                "X-CSRF-Token": token,
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then(() => this.props.history.push("/recipes"))
+            .catch(error => console.log(error.message));
     }
 
     componentDidMount() {
@@ -80,7 +106,7 @@ class Recipe extends React.Component {
                             />
                         </div>
                         <div className="col-sm-12 col-lg-2">
-                            <button type="button" className="btn btn-danger">
+                            <button type="button" className="btn btn-danger" onClick={this.deleteRecipe}>
                                 Delete Recipe
                             </button>
                         </div>
